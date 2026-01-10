163a5613bbaa83546cbadac7254f226bcfc5934d
Severin Hanke <severin.hanke@outlook.de>
2026-01-10 15:47:17 +0100

    Updated packages

--- src/constants/RouteNames.tsx
+++ src/constants/RouteNames.tsx
@@ -1,10 +1,6 @@
 export class RouteNames {
     static aboutMe: string = "/about-me";
     static links: string = "/links";
-    static gallery: string = "/gallery";
-    static galerieBadKreuznach: string = RouteNames.gallery + "/Bad-Kreuznach";
-    static galerieBernkastelKues: string = RouteNames.gallery + "/Bernkastel-Kues";
-    static galerieWaldalgesheim: string = RouteNames.gallery + "/Waldalgesheim";
     static shCoding: string = "/SH-Coding";
     static shCodingPokeManager: string = RouteNames.shCoding + "/Poke-Manager";
     static shCodingCountdowns: string = RouteNames.shCoding + "/Countdowns";
